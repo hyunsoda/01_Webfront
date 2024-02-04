f8207d9a85ce265983e783e9b4cc7aaec69bcd99
hyunsoda <so0144@naver.com>
2024-02-04 20:27:45 +0900

    fix

--- 3_Javascript/js/excercise1.js
+++ 3_Javascript/js/excercise1.js
@@ -1,52 +1,52 @@
-const numberbtn = document.querySelectorAll(".number");
-const result = document.getElementById("result");
-const reset = document.getElementById("reset");
+// const numberbtn = document.querySelectorAll(".number");
+// const result = document.getElementById("result");
+// const reset = document.getElementById("reset");
 
 
 
-let str="";
+// let str="";
 
-const regExp=/^\d{,10}$/;
+// const regExp=/^\d{,10}$/;
 
-for(let i=0; i<numberbtn.length-1;i++){
+// for(let i=0; i<numberbtn.length-1;i++){
         
-       numberbtn[i].addEventListener("click",() =>{
-        numberbtn[i] = i+1;
-        str+=(i+1);
+//        numberbtn[i].addEventListener("click",() =>{
+//         numberbtn[i] = i+1;
+//         str+=(i+1);
 
  
-        result.innerText=str;
+//         result.innerText=str;
       
 
-       });
-        };
+//        });
+//         };
         
-       numberbtn[9].addEventListener("click",() => {
+//        numberbtn[9].addEventListener("click",() => {
         
-        numberbtn[9].value='0';
-        str+= numberbtn[9].value;
-        result.innerText=str;
+//         numberbtn[9].value='0';
+//         str+= numberbtn[9].value;
+//         result.innerText=str;
 
         
-       });
+//        });
 
-//   if(regExp.test(Number(result.value))){
+// //   if(regExp.test(Number(result.value))){
           
-//   } else{
-//   alert("10자 초과입니다.")
-//   }
+// //   } else{
+// //   alert("10자 초과입니다.")
+// //   }
         
    
 
     
-    reset.addEventListener("click",()=>{
-    result.innerText="";
-    str="";
+//     reset.addEventListener("click",()=>{
+//     result.innerText="";
+//     str="";
      
-    return;
+//     return;
       
 
-   });
+//    });
 
 //    if (regExp.test(result.innerText)){
 //        result.innerText=str;
@@ -55,4 +55,22 @@ for(let i=0; i<numberbtn.length-1;i++){
 //    };
 
 
- 
+
+
+// 예시답안
+const numberList = document.getElementsByClassName("number");
+const result = document.getElementById("result");
+const reset = document.getElementById("reset");
+
+for(let number of numberList){
+       number.addEventListener("click",e => {
+              if (result.innerText.length , 10){
+                     result.innerText += e.target.innerText;
+              } else{
+                     alert("10자 까지만 입력할 수 있습니다");
+              }
+       });
+}
+reset.addEventListener("click",()=> {
+       result.innerText = "";
+})
